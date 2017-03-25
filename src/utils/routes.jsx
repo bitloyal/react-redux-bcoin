fac16d9437194ad8842078fd19f93c0dbec73bd8
Alex Trzeciak <trebek08@gmail.com>
2017-03-25 13:52:17 -0700

    added handling for login to work

--- src/utils/routes.jsx
+++ src/utils/routes.jsx
@@ -1,4 +1,42 @@
-import axios from 'axios';
+import axios from 'axios'; 
+
+
+export function signup(username, password, confirm){
+  if(password === confirm){
+    return axios({
+      method: 'post',
+      url: '/signup',
+      data: {
+        'username': username,
+        'password': password,
+        'confirm': confirm
+      }
+    }).then((resp)=>{
+      if(resp.data === 'already in database'){
+        this.setState({
+          message: "username already taken"
+        });
+      }else if(resp.data === "Error: username already taken"){
+        this.setState({
+          message: "Error: username already taken"
+        });
+      }else{
+        this.setState({
+          message: "Username created successfully!"
+        });
+      }
+    }).catch((err)=>{
+      this.setState({
+        message: "Error in Signup",
+        error: err
+      });
+    });
+  }else{
+    this.setState({
+      message: "Passwords Do Not Match" 
+    });
+  }   
+}
 
 export function login(username, password){
     return axios({
@@ -65,3 +103,4 @@ export function logout(){
         })
     })
 }
+
